ea89ae3870ca0f13184b9f442cb0c38e6433e669
Your Name <haciyevqurban97@gmail,com>
2023-11-11 02:27:47 +0400

    navbar ready

--- src/assets/index.js
+++ src/assets/index.js
@@ -29,7 +29,6 @@ import tesla from "./company/tesla.png";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
-
 export {
   logo,
   backend,
--- src/constants/index.js
+++ src/constants/index.js
@@ -23,7 +23,7 @@ import {
     jobit,
     tripguide,
     threejs,
-  } from "../assets";
+  } from "../assets/index.js";
   
   export const navLinks = [
     {
