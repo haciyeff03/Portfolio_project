967b1db69c7d112687bdc452f08aa73b6b17b8c8
Your Name <haciyevqurban97@gmail,com>
2023-11-11 15:41:31 +0400

    tailwind config solving

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,22 +2,24 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import {styles} from "../styles";
-import { logo } from '../assets';
 
 const Navbar = () => {
   const [active, setActive]= useState("")
   return (
     <nav
     className={`${styles.paddingX} w-full flex-items-center py-5 fixed top-0 z-20`}>
-      <div className='w-full flex justify-between items-center max-w-7xl mx-auto '>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link to="/"
-        className='flex items-center gap-2'
+        className="flex items-center gap-2"
         onClick={()=> {
           setActive("");
           window.scrollTo(0,0);
         }}>
-          <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
+          <p className='text-red-600 text-[18px] font-bold cursor-pointer'>Qurban Haciyev  
+          <span className='sm:block hidden'>| Web Developer </span> </p>
+          <p className='text-red-500'>asdca</p>
         </Link>
+       
       </div>
     </nav>
   )
