ca5a03c2b3b6c0d3a65946160aa802d9ddc19600
Your Name <haciyevqurban97@gmail.com>
2023-11-21 23:15:45 +0400

    special problems is fixed

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -11,7 +11,11 @@ const Computers = () => {
       <hemisphereLight intensity={0.15} groundColor="black"/>
       <pointLight intensity={1}/>
       <primitive 
-      object={computer.scene}/>
+      object={computer.scene}
+      scale={0.75}
+      position={[0,-3.25,-1.5]}
+      rotation={[-0.01,-0.2,-0.1]}
+      />
     </mesh>
   )
 }
