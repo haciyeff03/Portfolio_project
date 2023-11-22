aa5c01bf5c7fd0c06e2b9bf6014a026e23acda10
Your Name <haciyevqurban97@gmail.com>
2023-11-22 23:05:46 +0400

    about section completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 
-import { styles } from "../styles";
-import { services } from "../constants";
+import { styles } from "../";
+import { services } from "./constants";
 import {SectionWrapper} from "./hoc"
 import { fadeIn, textVariant } from "../utils/motion";
 
