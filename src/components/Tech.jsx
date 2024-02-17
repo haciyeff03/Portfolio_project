b8a97ffaec35dcbbca7192111795c00d871a0478
Qurban Haciyev <haciyevqurban97@gmail.com>
2024-02-17 21:20:15 +0400

    errors #1

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "./hoc";
 import { technologies } from "../constants";
-
 const Tech = () => {
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
